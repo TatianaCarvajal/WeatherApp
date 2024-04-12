2a6b22c8b936cfca9c5004a533779e5effaa8f73
Tatiana Carvajal <taticv159912@gmail.com>
2024-04-12 15:28:27 -0500

    first commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Tatiana Carvajal on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
