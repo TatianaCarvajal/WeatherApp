6a7173bd14500bf7bb4b4841ea320585208e262c
Tatiana Carvajal <taticv159912@gmail.com>
2024-04-23 17:30:49 -0500

    Creating weatherView

---
+++ WeatherApp/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  WeatherApp
+//
+//  Created by Tatiana Carvajal on 15/04/24.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
