4de043bd65443488fdd40d8228b2be171fa4e6b0
Tatiana Carvajal <taticv159912@gmail.com>
2024-05-21 16:09:27 -0500

    adding ProgressView

--- WeatherApp/View/WeatherView.swift
+++ WeatherApp/View/WeatherView.swift
@@ -12,64 +12,68 @@ struct WeatherView: View {
     
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.black, .blue, .white], startPoint: .topLeading, endPoint: .bottomLeading)
-                .edgesIgnoringSafeArea(.all)
-            if let weather = viewModel.weather {
-                VStack {
-                    Text(weather.name)
-                        .font(.system(size: 34, weight: .bold))
-                        .foregroundColor(.white)
-                        .padding(.top, 60)
-                    
-                    Spacer()
-                    
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 25)
-                            .trim(from: 0.0, to: 0.62)
+            if viewModel.isLoading {
+                ProgressView()
+            } else {
+                LinearGradient(colors: [.black, .blue, .white], startPoint: .topLeading, endPoint: .bottomLeading)
+                    .edgesIgnoringSafeArea(.all)
+                if let weather = viewModel.weather {
+                    VStack {
+                        Text(weather.name)
+                            .font(.system(size: 34, weight: .bold))
                             .foregroundColor(.white)
-                            .opacity(0.3)
-                            .frame(width: 342, height: 174, alignment: .bottom)
+                            .padding(.top, 60)
                         
-                        HStack(alignment: .bottom) {
+                        Spacer()
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 25)
+                                .trim(from: 0.0, to: 0.62)
+                                .foregroundColor(.white)
+                                .opacity(0.3)
+                                .frame(width: 342, height: 174, alignment: .bottom)
                             
-                            VStack(alignment: .leading, spacing: 8) {
-                                Text(weather.temperature)
-                                    .font(.system(size: 64))
+                            HStack(alignment: .bottom) {
                                 
-                                VStack(alignment: .leading, spacing: 2) {
-                                    Text(weather.latitude)
-                                    Text(weather.longitude)
+                                VStack(alignment: .leading, spacing: 8) {
+                                    Text(weather.temperature)
+                                        .font(.system(size: 64))
+                                    
+                                    VStack(alignment: .leading, spacing: 2) {
+                                        Text(weather.latitude)
+                                        Text(weather.longitude)
+                                    }
+                                    .font(.system(size: 20))
                                 }
-                                .font(.system(size: 20))
-                            }
-                            .padding(.horizontal)
-                            
-                            Spacer()
-                            
-                            VStack(spacing: 4) {
-                                switch weather.weatherType {
-                                case .clouds:
-                                    WeatherTypeImageView(weatherImage: "cloud.sun.fill")
-                                case .rain:
-                                    WeatherTypeImageView(weatherImage: "cloud.rain.fill")
-                                case .clear:
-                                    WeatherTypeImageView(weatherImage: "sun.max.fill")
-                                case .snow:
-                                    WeatherTypeImageView(weatherImage: "cloud.snow.fill")
-                                }
+                                .padding(.horizontal)
                                 
-                                Text(weather.weatherDescription)
-                                    .font(.system(size: 16))
+                                Spacer()
+                                
+                                VStack(spacing: 4) {
+                                    switch weather.weatherType {
+                                    case .clouds:
+                                        WeatherTypeImageView(weatherImage: "cloud.sun.fill")
+                                    case .rain:
+                                        WeatherTypeImageView(weatherImage: "cloud.rain.fill")
+                                    case .clear:
+                                        WeatherTypeImageView(weatherImage: "sun.max.fill")
+                                    case .snow:
+                                        WeatherTypeImageView(weatherImage: "cloud.snow.fill")
+                                    }
+                                    
+                                    Text(weather.weatherDescription)
+                                        .font(.system(size: 16))
+                                }
+                                .padding(.trailing, 30)
                             }
-                            .padding(.trailing, 30)
+                            .foregroundColor(.black)
+                            .padding(.leading, 26)
                         }
-                        .foregroundColor(.black)
-                        .padding(.leading, 26)
+                        Spacer()
+                        WeatherInformationView(minTemperature: weather.minTemperature, maxTemperature: weather.maxTemperature, pressure: weather.pressure, humidity: weather.humidity)
                     }
-                    Spacer()
-                    WeatherInformationView(minTemperature: weather.minTemperature, maxTemperature: weather.maxTemperature, pressure: weather.pressure, humidity: weather.humidity)
+                    .edgesIgnoringSafeArea(.bottom)
                 }
-                .edgesIgnoringSafeArea(.bottom)
             }
         }
         .alert(isPresented: $viewModel.isShowingError, error: viewModel.error) { error in
